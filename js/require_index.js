f221e083d97093db318494d8ee89f7ab1d94516c
Jessy Landas <jessylandas59.jl@gmail.com>
2020-12-04 17:24:25 +0100

    ajout fichier js

---
+++ js/require_index.js
@@ -0,0 +1,15 @@
+fetch("asset/header.html")
+.then(response => {
+    return response.text()
+})
+.then(data => {
+    document.querySelector("header").innerHTML = data;
+});
+
+fetch("asset/footer.html")
+.then(response => {
+    return response.text()
+})
+.then(data => {
+    document.querySelector("footer").innerHTML = data;
+});
