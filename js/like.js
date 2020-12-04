552c8a6dd3c5939d2e662bdebf743f75a98e4bd2
Jessy Landas <jessylandas59.jl@gmail.com>
2020-12-04 11:45:31 +0100

    ajout like

--- js/like.js
+++ js/like.js
@@ -1,15 +1,21 @@
 
 var like = false;
 var nblike = 95;
+var angle = 0;
 
 var like_user = document.getElementById('pourcentage');
-var like_button = document.getElementById('pouce').addEventListener("click", function(){
+var like_button = document.getElementById('pouce')
+like_button.addEventListener("click", function(){
 
     if(like == false){
+        angle += 540;
+        like_button.style.transform = "rotate(" + angle +"deg)"
         nblike += 1
         like_user.innerText=  nblike + "%";
         like = true;
     }else{
+        angle -= 540;
+        like_button.style.transform = "rotate(" + angle +"deg)"
         nblike -= 1
         like_user.innerText=  nblike + "%";
         like = false;
